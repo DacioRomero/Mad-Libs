a362241560cc6e7cfefb0d53d4809baf8df7ba74
Dacio Romero <dacioromero@gmail.com>
2018-08-31 10:49:33 -0700

    Update MVP

--- mad-libs.py
+++ mad-libs.py
@@ -1,2 +1,17 @@
+import re
+
+def print_story(story, vars=[]):
+    new_format = ''.join('underscore' if c.isdigit() and int(c) >= len(vars) else c for c in story)
+    print(new_format.format(*vars, underscore="___"))
+
+def main():
+    story = "There was once a {0} that had a {1}."
+    print_story(story)
+
+    vars = []
+    for _ in range(story.count('{')):
+        vars.append(input("Enter a word: "))
+        print_story(story, vars)
+
 if __name__ == '__main__':
-    print("There was once a {} who ate a/an {}. This made them have {}".format(input(), input(), input()))
+    main()
