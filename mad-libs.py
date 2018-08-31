c15f9bfa96ce9adaffb668f173346311b171fa48
Dacio Romero <dacioromero@gmail.com>
2018-08-31 11:06:43 -0700

    Replace double quotes with single quotes

--- mad-libs.py
+++ mad-libs.py
@@ -2,15 +2,15 @@ import re
 
 def print_story(story, vars=[]):
     new_format = ''.join('underscore' if c.isdigit() and int(c) >= len(vars) else c for c in story)
-    print(new_format.format(*vars, underscore="___"))
+    print(new_format.format(*vars, underscore='___'))
 
 def main():
-    story = "There was once a {0} that had a {1}."
+    story = 'There was once a {0} that had a {1}.'
     print_story(story)
 
     vars = []
     for _ in range(story.count('{')):
-        vars.append(input("Enter a word: "))
+        vars.append(input('Enter a word: '))
         print_story(story, vars)
 
 if __name__ == '__main__':
