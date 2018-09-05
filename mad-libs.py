d1615172eb06ae4abe3ff7410b9035e7fc7575bb
Dacio Romero <dacioromero@gmail.com>
2018-09-05 10:30:53 -0700

    Use dictionary, comprehension, regex

--- mad-libs.py
+++ mad-libs.py
@@ -1,17 +1,14 @@
 import re
 
-def print_story(story, vars=[]):
-    new_format = ''.join('underscore' if c.isdigit() and int(c) >= len(vars) else c for c in story)
-    print(new_format.format(*vars, underscore='___'))
+SUBSTITUTOR = re.compile(r"(\[[a-z]+?\])")
 
 def main():
-    story = 'There was once a {0} that had a {1}.'
-    print_story(story)
-
-    vars = []
-    for _ in range(story.count('{')):
-        vars.append(input('Enter a word: '))
-        print_story(story, vars)
+    story = 'There was once a(n) [noun] that had a(n) [verb].'
+    word_dict = {t: [input("Enter a(n) {}: ".format(t[1:-1])) for _ in range(story.count(t))]
+                 for t in set(re.findall(SUBSTITUTOR, story))}
+    formatted_story = ''.join(word_dict[sec].pop(0) if sec in word_dict else sec
+                              for sec in re.split(SUBSTITUTOR, story))
+    print(formatted_story)
 
 if __name__ == '__main__':
     main()
